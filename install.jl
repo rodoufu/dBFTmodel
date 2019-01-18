8009d1236308947b7a1974c58cb5cf2d52356580
Rodolfo P A <rodoufu@gmail.com>
2019-01-18 16:02:52 -0200

    Create install.jl

---
+++ install.jl
@@ -0,0 +1,22 @@
+using Pkg
+
+Pkg.add("JuMP")
+Pkg.add("Test")
+Pkg.add("Compat")
+Pkg.add("MathOptInterface")
+Pkg.add("GLPK")
+Pkg.add("GLPKMathProgInterface")
+Pkg.add("LinearFractional")
+
+Pkg.add("Cbc")
+Pkg.add("Clp")
+Pkg.add("ECOS")
+Pkg.add("Ipopt")
+Pkg.add("NLopt")
+Pkg.add("SCS")
+# Pkg.add("CPLEX") # Needs working installation
+# Pkg.add("Gurobi") # Needs working installation
+Pkg.add("AmplNLWriter")
+# Pkg.add("CoinOptServices")
+
+Pkg.update()
